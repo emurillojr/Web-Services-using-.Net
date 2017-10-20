6ec9b6e337f5577b6b4de751f01b2bf5c026deba
Ernesto Murillo <emurillojr@users.noreply.github.com>
2017-10-20 18:34:03 -0400

    Added  - Reading a File With XPath

---
+++ ReadFileFromXpath/ReadFileFromXpath/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ReadFileFromXpath
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Version Info";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(135, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "All Contacts";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(238, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Only Males";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(135, 32);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(146, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Search Specific Last Name";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.Location = new System.Drawing.Point(135, 6);
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.Size = new System.Drawing.Size(146, 20);
+            this.lastNameText.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter Last Name";
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(32, 124);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResults.Size = new System.Drawing.Size(281, 200);
+            this.txtResults.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 347);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lastNameText);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox lastNameText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResults;
+    }
+}
+
---
+++ ReadFileFromXpath/ReadFileFromXpath/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace ReadFileFromXpath
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtResults.Text = "";
+ 
+            XPathNavigator nav;
+            XPathDocument docNav;
+
+            String strExpression;
+            String strExpression1;
+            strExpression = "/ContactDatabase/DBinfo/LatestVers";
+            strExpression1 = "/ContactDatabase/DBinfo/CurrentVersion";
+            // Open the XML.
+            string xmlFile = Application.StartupPath + "\\contacts.xml";
+            docNav = new XPathDocument(xmlFile);
+
+            // Create a navigator to query with XPath.
+            nav = docNav.CreateNavigator();
+
+            String version = nav.SelectSingleNode(strExpression).Value;
+            String version2 = nav.SelectSingleNode(strExpression1).Value;
+
+            txtResults.Text += version + Environment.NewLine;
+            txtResults.Text += version2 + Environment.NewLine;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtResults.Text = "";
+
+            XPathNavigator nav;
+            XPathDocument docNav;
+            XPathNodeIterator NodeIter;
+
+            String strExpression;
+            strExpression = "/ContactDatabase/DBinfo/Contact";
+
+            // Open the XML.
+            string xmlFile = Application.StartupPath + "\\contacts.xml";
+            docNav = new XPathDocument(xmlFile);
+
+            // Create a navigator to query with XPath.
+            nav = docNav.CreateNavigator();
+            
+            NodeIter = nav.Select(strExpression);
+
+            while (NodeIter.MoveNext())
+            {
+                txtResults.Text += "Gender: " + NodeIter.Current.SelectSingleNode("./@sex").Value + Environment.NewLine; ;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("FirstName").Value + Environment.NewLine; ;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("LastName").Value + Environment.NewLine; ;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("PhoneNumber").Value + Environment.NewLine; ;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("FaxNumber").Value + Environment.NewLine; ;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("emailAddress").Value + Environment.NewLine; ;
+                txtResults.Text += Environment.NewLine;
+
+            };
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtResults.Text = "";
+
+            XPathNavigator nav;
+            XPathDocument docNav;
+            XPathNodeIterator NodeIter;
+
+            String strExpression;
+            strExpression = "/ContactDatabase/DBinfo/Contact[@sex = 'm']";
+
+            // Open the XML.
+            string xmlFile = Application.StartupPath + "\\contacts.xml";
+            docNav = new XPathDocument(xmlFile);
+
+            // Create a navigator to query with XPath.
+            nav = docNav.CreateNavigator();
+
+            NodeIter = nav.Select(strExpression);
+
+            while (NodeIter.MoveNext())
+            {
+                txtResults.Text += "Gender: " + NodeIter.Current.SelectSingleNode("./@sex").Value + Environment.NewLine;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("FirstName").Value + Environment.NewLine;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("LastName").Value + Environment.NewLine;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("PhoneNumber").Value + Environment.NewLine;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("FaxNumber").Value + Environment.NewLine;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("emailAddress").Value + Environment.NewLine;
+                txtResults.Text += Environment.NewLine;
+            };
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtResults.Text = "";
+
+            XPathNavigator nav;
+            XPathDocument docNav;
+            XPathNodeIterator NodeIter;
+
+            String strExpression;
+            strExpression = "/ContactDatabase/DBinfo/Contact[LastName[contains(text(),'" + lastNameText.Text.ToString() + "')]]";
+
+            // Open the XML.
+            string xmlFile = Application.StartupPath + "\\contacts.xml";
+            docNav = new XPathDocument(xmlFile);
+
+            // Create a navigator to query with XPath.
+            nav = docNav.CreateNavigator();
+            
+            NodeIter = nav.Select(strExpression);
+            
+            while (NodeIter.MoveNext())
+            {
+                txtResults.Text += "Gender: " + NodeIter.Current.SelectSingleNode("./@sex").Value + Environment.NewLine;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("FirstName").Value + Environment.NewLine;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("LastName").Value + Environment.NewLine;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("PhoneNumber").Value + Environment.NewLine;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("FaxNumber").Value + Environment.NewLine;
+                txtResults.Text += NodeIter.Current.SelectSingleNode("emailAddress").Value + Environment.NewLine;
+                txtResults.Text += Environment.NewLine;
+
+            };
+        }
+    }
+}
