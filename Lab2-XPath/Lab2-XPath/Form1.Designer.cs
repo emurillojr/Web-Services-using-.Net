6665d011beff31ca922960cc96d2cb184608b543
Ernesto Murillo <emurillojr@users.noreply.github.com>
2017-10-20 19:00:04 -0400

    Added Lab 2- Using XPath

---
+++ Lab2-XPath/Lab2-XPath/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Lab2_XPath
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.btnGetOrder = new System.Windows.Forms.Button();
+            this.txtboxItemInfo = new System.Windows.Forms.TextBox();
+            this.txtboxShippingInfo = new System.Windows.Forms.TextBox();
+            this.txtboxBillingInfo = new System.Windows.Forms.TextBox();
+            this.lblTotalCostOfOrder = new System.Windows.Forms.Label();
+            this.lblNumberOfItemsBought = new System.Windows.Forms.Label();
+            this.btnItemInformation = new System.Windows.Forms.Button();
+            this.btnShippingInformation = new System.Windows.Forms.Button();
+            this.btnBillingInformation = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Location = new System.Drawing.Point(661, 271);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(75, 23);
+            this.btnClearAll.TabIndex = 22;
+            this.btnClearAll.Text = "Clear All";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // btnGetOrder
+            // 
+            this.btnGetOrder.Location = new System.Drawing.Point(661, 36);
+            this.btnGetOrder.Name = "btnGetOrder";
+            this.btnGetOrder.Size = new System.Drawing.Size(75, 23);
+            this.btnGetOrder.TabIndex = 21;
+            this.btnGetOrder.Text = "Get Order";
+            this.btnGetOrder.UseVisualStyleBackColor = true;
+            this.btnGetOrder.Click += new System.EventHandler(this.btnGetOrder_Click);
+            // 
+            // txtboxItemInfo
+            // 
+            this.txtboxItemInfo.Location = new System.Drawing.Point(414, 58);
+            this.txtboxItemInfo.Multiline = true;
+            this.txtboxItemInfo.Name = "txtboxItemInfo";
+            this.txtboxItemInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtboxItemInfo.Size = new System.Drawing.Size(173, 236);
+            this.txtboxItemInfo.TabIndex = 20;
+            // 
+            // txtboxShippingInfo
+            // 
+            this.txtboxShippingInfo.Location = new System.Drawing.Point(220, 58);
+            this.txtboxShippingInfo.Multiline = true;
+            this.txtboxShippingInfo.Name = "txtboxShippingInfo";
+            this.txtboxShippingInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtboxShippingInfo.Size = new System.Drawing.Size(173, 236);
+            this.txtboxShippingInfo.TabIndex = 19;
+            // 
+            // txtboxBillingInfo
+            // 
+            this.txtboxBillingInfo.Location = new System.Drawing.Point(18, 58);
+            this.txtboxBillingInfo.Multiline = true;
+            this.txtboxBillingInfo.Name = "txtboxBillingInfo";
+            this.txtboxBillingInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtboxBillingInfo.Size = new System.Drawing.Size(173, 236);
+            this.txtboxBillingInfo.TabIndex = 18;
+            // 
+            // lblTotalCostOfOrder
+            // 
+            this.lblTotalCostOfOrder.AutoSize = true;
+            this.lblTotalCostOfOrder.Location = new System.Drawing.Point(615, 141);
+            this.lblTotalCostOfOrder.Name = "lblTotalCostOfOrder";
+            this.lblTotalCostOfOrder.Size = new System.Drawing.Size(35, 13);
+            this.lblTotalCostOfOrder.TabIndex = 17;
+            this.lblTotalCostOfOrder.Text = "label2";
+            // 
+            // lblNumberOfItemsBought
+            // 
+            this.lblNumberOfItemsBought.AutoSize = true;
+            this.lblNumberOfItemsBought.Location = new System.Drawing.Point(615, 111);
+            this.lblNumberOfItemsBought.Name = "lblNumberOfItemsBought";
+            this.lblNumberOfItemsBought.Size = new System.Drawing.Size(35, 13);
+            this.lblNumberOfItemsBought.TabIndex = 16;
+            this.lblNumberOfItemsBought.Text = "label1";
+            // 
+            // btnItemInformation
+            // 
+            this.btnItemInformation.Location = new System.Drawing.Point(433, 12);
+            this.btnItemInformation.Name = "btnItemInformation";
+            this.btnItemInformation.Size = new System.Drawing.Size(137, 23);
+            this.btnItemInformation.TabIndex = 15;
+            this.btnItemInformation.Text = "Item Information";
+            this.btnItemInformation.UseVisualStyleBackColor = true;
+            this.btnItemInformation.Click += new System.EventHandler(this.btnItemInformation_Click);
+            // 
+            // btnShippingInformation
+            // 
+            this.btnShippingInformation.Location = new System.Drawing.Point(238, 12);
+            this.btnShippingInformation.Name = "btnShippingInformation";
+            this.btnShippingInformation.Size = new System.Drawing.Size(137, 23);
+            this.btnShippingInformation.TabIndex = 14;
+            this.btnShippingInformation.Text = "Shipping Information";
+            this.btnShippingInformation.UseVisualStyleBackColor = true;
+            this.btnShippingInformation.Click += new System.EventHandler(this.btnShippingInformation_Click);
+            // 
+            // btnBillingInformation
+            // 
+            this.btnBillingInformation.Location = new System.Drawing.Point(35, 12);
+            this.btnBillingInformation.Name = "btnBillingInformation";
+            this.btnBillingInformation.Size = new System.Drawing.Size(137, 23);
+            this.btnBillingInformation.TabIndex = 13;
+            this.btnBillingInformation.Text = "Billing Information";
+            this.btnBillingInformation.UseVisualStyleBackColor = true;
+            this.btnBillingInformation.Click += new System.EventHandler(this.btnBillingInformation_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 317);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.btnGetOrder);
+            this.Controls.Add(this.txtboxItemInfo);
+            this.Controls.Add(this.txtboxShippingInfo);
+            this.Controls.Add(this.txtboxBillingInfo);
+            this.Controls.Add(this.lblTotalCostOfOrder);
+            this.Controls.Add(this.lblNumberOfItemsBought);
+            this.Controls.Add(this.btnItemInformation);
+            this.Controls.Add(this.btnShippingInformation);
+            this.Controls.Add(this.btnBillingInformation);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Button btnGetOrder;
+        private System.Windows.Forms.TextBox txtboxItemInfo;
+        private System.Windows.Forms.TextBox txtboxShippingInfo;
+        private System.Windows.Forms.TextBox txtboxBillingInfo;
+        private System.Windows.Forms.Label lblTotalCostOfOrder;
+        private System.Windows.Forms.Label lblNumberOfItemsBought;
+        private System.Windows.Forms.Button btnItemInformation;
+        private System.Windows.Forms.Button btnShippingInformation;
+        private System.Windows.Forms.Button btnBillingInformation;
+    }
+}
+
---
+++ Lab2-XPath/Lab2-XPath/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace Lab2_XPath
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnItemInformation_Click(object sender, EventArgs e)
+        {
+            txtboxItemInfo.Text = "";
+            XPathNavigator nav;
+            XPathDocument docNav;
+            XPathNodeIterator NodeIter;
+            String strExpression;
+            strExpression = "/Order/Items/Item";
+            // Open the XML.
+            string xmlFile = Application.StartupPath + "\\OrderInfo.xml";
+            docNav = new XPathDocument(xmlFile);
+            // Create a navigator to query with XPath.
+            nav = docNav.CreateNavigator();
+            NodeIter = nav.Select(strExpression);
+            StringBuilder itemResult = new StringBuilder();
+
+            while (NodeIter.MoveNext())
+            {
+                XPathNodeIterator custOptionIter;
+                //XPathNodeIterator customerOptions = NodeIter.Current.SelectChildren(XPathNodeType.Element);
+                itemResult.AppendLine(NodeIter.Current.SelectSingleNode("PartNo").Value);
+                itemResult.AppendLine(NodeIter.Current.SelectSingleNode("Description").Value);
+                itemResult.AppendLine(NodeIter.Current.SelectSingleNode("UnitPrice").Value);
+                itemResult.AppendLine(NodeIter.Current.SelectSingleNode("Quantity").Value);
+                itemResult.AppendLine(NodeIter.Current.SelectSingleNode("TotalCost").Value);
+
+                custOptionIter = NodeIter.Current.Select("CustomerOptions");
+                while (custOptionIter.MoveNext())
+                {
+                    XPathNavigator custOptNav = custOptionIter.Current;
+                    int count = custOptionIter.Current.SelectChildren(XPathNodeType.Element).Count;
+                    if (count > 0)
+                    {
+                        itemResult.AppendLine("-- Customer Options --");
+                        itemResult.AppendLine("Size: " + custOptNav.SelectSingleNode("Size").Value);
+                        itemResult.AppendLine("Color: " + custOptNav.SelectSingleNode("Color").Value);
+                    }
+                }
+
+                itemResult.AppendLine("");
+                txtboxItemInfo.Text = itemResult.ToString();
+            };
+
+            //double Total = (double)nav.Evaluate("sum(Order/Items/Item/TotalCost)");
+            //double Count = (double)nav.Evaluate("count(Order/Items/Item)");
+            //lblTotalCostOfOrder.Text = "Total Orders:    " + Total.ToString();
+            //lblNumberOfItemsBought.Text = "Numbers of items:" + Count.ToString();
+
+            lblTotalCostOfOrder.Text = "Total Orders:    " + nav.Evaluate("sum(Order/Items/Item/TotalCost)");
+            lblNumberOfItemsBought.Text = "Numbers of items:" + nav.Evaluate("count(Order/Items/Item)");
+            
+        }
+
+        private void btnShippingInformation_Click(object sender, EventArgs e)
+        {
+            txtboxShippingInfo.Text = "";
+            XPathNavigator nav;
+            XPathDocument docNav;
+            XPathNodeIterator NodeIter;
+            String strExpression;
+            strExpression = "/Order/ShippingInformation";
+            // Open the XML.
+            string xmlFile = Application.StartupPath + "\\OrderInfo.xml";
+            docNav = new XPathDocument(xmlFile);
+            // Create a navigator to query with XPath.
+            nav = docNav.CreateNavigator();
+            NodeIter = nav.Select(strExpression);
+
+            while (NodeIter.MoveNext())
+            {
+                txtboxShippingInfo.Text += NodeIter.Current.SelectSingleNode("Address").Value + Environment.NewLine;
+                txtboxShippingInfo.Text += NodeIter.Current.SelectSingleNode("City").Value + Environment.NewLine;
+                txtboxShippingInfo.Text += NodeIter.Current.SelectSingleNode("State").Value + Environment.NewLine;
+                txtboxShippingInfo.Text += NodeIter.Current.SelectSingleNode("ZipCode").Value + Environment.NewLine;
+                txtboxShippingInfo.Text += Environment.NewLine;
+
+            };
+
+
+        }
+
+        private void btnBillingInformation_Click(object sender, EventArgs e)
+        {
+            txtboxBillingInfo.Text = "";
+            XPathNavigator nav;
+            XPathDocument docNav;
+            XPathNodeIterator NodeIter;
+            String strExpression;
+            strExpression = "/Order/BillingInformation";
+            // Open the XML.
+            string xmlFile = Application.StartupPath + "\\OrderInfo.xml";
+            docNav = new XPathDocument(xmlFile);
+            // Create a navigator to query with XPath.
+            nav = docNav.CreateNavigator();
+            NodeIter = nav.Select(strExpression);
+
+            while (NodeIter.MoveNext())
+            {
+                txtboxBillingInfo.Text += NodeIter.Current.SelectSingleNode("Name").Value + Environment.NewLine;
+                txtboxBillingInfo.Text += NodeIter.Current.SelectSingleNode("Address").Value + Environment.NewLine;
+                txtboxBillingInfo.Text += NodeIter.Current.SelectSingleNode("City").Value + Environment.NewLine;
+                txtboxBillingInfo.Text += NodeIter.Current.SelectSingleNode("State").Value + Environment.NewLine;
+                txtboxBillingInfo.Text += NodeIter.Current.SelectSingleNode("ZipCode").Value + Environment.NewLine;
+                txtboxBillingInfo.Text += Environment.NewLine;
+
+            };
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            txtboxItemInfo.Text = "";
+            txtboxShippingInfo.Text = "";
+            txtboxBillingInfo.Text = "";
+            lblTotalCostOfOrder.Text = "";
+            lblNumberOfItemsBought.Text = "";
+        }
+
+        private void btnGetOrder_Click(object sender, EventArgs e)
+        {
+            btnItemInformation_Click(sender, e);
+            btnShippingInformation_Click(sender, e);
+            btnBillingInformation_Click(sender, e);
+
+        }
+    }
+}
