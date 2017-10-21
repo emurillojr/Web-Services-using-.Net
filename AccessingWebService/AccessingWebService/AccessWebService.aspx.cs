dde658fc152a762e01fc4fb46a579c80b2232c17
Ernesto Murillo <emurillojr@users.noreply.github.com>
2017-10-20 20:21:34 -0400

    added In class 1 Access Web Service

---
+++ AccessingWebService/AccessingWebService/AccessWebService.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AccessingWebService
+{
+    public partial class AccessWebService : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //create an object of Web Reference
+            Phone.CDyne.PhoneVerify myPhone = new Phone.CDyne.PhoneVerify();
+            Phone.CDyne.PhoneReturn myReturn = new Phone.CDyne.PhoneReturn();
+
+            myReturn = myPhone.CheckPhoneNumber(TextBox1.Text, "0");
+            Label3.Text = myReturn.TelecomCity + ", " + myReturn.TelecomState + " " + myReturn.TelecomZip;
+        }
+    }
+}
