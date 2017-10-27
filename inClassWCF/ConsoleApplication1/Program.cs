a32fbe7e545cbb82b62803362af88827fe038d3b
Ernesto Murillo <emurillojr@users.noreply.github.com>
2017-10-27 12:59:27 -0400

    WCF Service / Client Program

---
+++ inClassWCF/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel; // added 
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContactExample.ContactManagerClient myProxy = new ContactExample.ContactManagerClient();  // added
+            int num = myProxy.GetNumberOfContacts();
+            
+            //project name. service reference .  class name from other project VARIABLE =  myProxy.GetClientInformation();
+            ConsoleApplication1.ContactExample.ContactInformation names = myProxy.GetClientInformation();
+            //Console.WriteLine(names.FirstName);
+            //Console.WriteLine(names.LastName);
+            //Console.WriteLine(names.PhoneNumber);
+            Console.WriteLine("The fax number is: " + names.FaxNumber);
+            //Console.WriteLine(names.EmailAddress);
+
+            Console.WriteLine("The number of contacts are: " + num);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ inClassWCF/inClassWCF/IContactManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+// interface
+
+namespace ContactManager
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IContactManager
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+
+        [OperationContract]
+        int GetNumberOfContacts();  // added for in class
+
+        [OperationContract]
+        ContactInformation GetClientInformation();  // added for in class
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        [DataMember]
+        public bool BoolValue = true;
+        [DataMember]
+        public string StringValue = "Hello ";
+
+    }
+
+    [DataContract]
+    public class ContactInformation
+    {
+        [DataMember]
+        public string FirstName;
+        [DataMember]
+        public string LastName;
+        [DataMember]
+        public string PhoneNumber;
+        [DataMember]
+        public string FaxNumber;
+        [DataMember]
+        public string EmailAddress;
+
+    }
+
+}
